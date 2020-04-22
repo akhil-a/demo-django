56db9439cbe0c887f30128e3d139cc867dc0db8f
EC2 Default User <ec2-user@ip-172-31-28-101.us-east-2.compute.internal>
2020-04-22 09:03:37 +0000

    changes added

--- sas_aaa/webSAS/settings.py
+++ sas_aaa/webSAS/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = '_!w(n2_@iy9+ofz8h#%d57+x9czs@3l8%(rvv8fa(z24)_lq*e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.16.3.98', 'localhost', '192.168.3.96', '192.168.43.245', '127.0.0.1', '172.16.3.71', '0.0.0.0',
-                 '47cfa226.ngrok.io']
+ALLOWED_HOSTS = ['18.217.120.17']
 
 # Application definition
 
